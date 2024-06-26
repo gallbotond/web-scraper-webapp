c3106def3c017938f4357fd4e3d283291c5c764d
gallbotond <gallbotondofc@gmail.com>
2024-06-26 23:58:48 +0300

    diagram

--- backend/helper/tokenHelper.js
+++ backend/helper/tokenHelper.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from "../config.js";
 
 // token functions
 export function generateToken(email) {
-    return jwt.sign({ email }, ACCESS_TOKEN_SECRET, { expiresIn: "25s" });
+    return jwt.sign({ email }, ACCESS_TOKEN_SECRET, { expiresIn: "55m" });
 }
 
 export function generateRefreshToken(email) {
--- backend/routes/itemRoute.js
+++ backend/routes/itemRoute.js
@@ -11,10 +11,7 @@ router.get("/", authenticateToken, async (req, res) => {
     //   res.send("Get all items");
     try {
         const items = await Item.find();
-        res.status(200).json({
-            count: items.length,
-            data: items,
-        });
+        res.status(200).json(items);
     } catch (error) {
         console.error("Error getting items", error);
     }
